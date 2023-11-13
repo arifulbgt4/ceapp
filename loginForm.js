445872dcc092b9ec06af6b32f8c414c1f1eba025
jbubu97 <jabbar.babu81@gmail.com>
2023-11-14 00:20:32 +0600

    Added input type and placeholder

--- loginForm.js
+++ loginForm.js
@@ -4,7 +4,11 @@ const loginForm = document.createElement('form');
 form.appendChild(loginForm);
 
 const userNameInput = document.createElement('input');
+userNameInput.setAttribute('type', 'text');
+userNameInput.placeholder = 'User Name';
 const passwordInput = document.createElement('input');
+passwordInput.setAttribute('type', 'password');
+passwordInput.placeholder ='Password';
 const loginBtn = document.createElement('button');
 loginBtn.textContent = 'Login'
 
