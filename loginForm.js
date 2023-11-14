6641dcc4c4363438d421b45e06acead18f613b44
jbubu97 <jabbar.babu81@gmail.com>
2023-11-14 15:42:17 +0600

    added login click function

--- loginForm.js
+++ loginForm.js
@@ -1,17 +1,30 @@
 const form = document.getElementById('form');
 
 const loginForm = document.createElement('form');
+loginForm.setAttribute('class', 'form');
 form.appendChild(loginForm);
-
-const userNameInput = document.createElement('input');
-userNameInput.setAttribute('type', 'text');
-userNameInput.placeholder = 'User Name';
+const tittle = document.createElement('h1');
+tittle.setAttribute('class', 'tittle');
+tittle.textContent = `User Login`;
+loginForm.appendChild(tittle);
+const emailInput = document.createElement('input');
+emailInput.setAttribute('type', 'email');
+emailInput.setAttribute('id', 'emailInput');
+emailInput.placeholder = 'Your Email';
 const passwordInput = document.createElement('input');
 passwordInput.setAttribute('type', 'password');
+passwordInput.setAttribute('id', 'passInput');
 passwordInput.placeholder ='Password';
 const loginBtn = document.createElement('button');
-loginBtn.textContent = 'Login'
+loginBtn.setAttribute('type', 'submit')
+loginBtn.setAttribute('class', 'loginBtn');
+loginBtn.setAttribute('onclick', 'handleLogin()');
+loginBtn.textContent = 'Login';
 
-loginForm.appendChild(userNameInput);
+loginForm.appendChild(emailInput);
 loginForm.appendChild(passwordInput);
 loginForm.appendChild(loginBtn);
+
+function handleLogin(){
+    console.log(`Login Success!`);
+}
